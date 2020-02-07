2f92bd2e760327984d0de259ac7130ecbdb69a55
Jason Tamez <druidic@gmail.com>
2020-02-07 13:09:01 -0600

    templatizing index +index.pug +comments in server.js +open the door to using pug files instead of xml files in the future

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const express = require("express");
 const fs = require('fs');
 const glob = require('glob');
 const app = express();
+const pug = require('pug');
 var rulesets = new Map();
 var debug = [];
 // we've started you off with Express,
@@ -15,11 +16,6 @@ var debug = [];
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-	response.sendFile(__dirname + "/views/index.html");
-});
 
 
 // This section scans for "master" XML files and saves them to the map rulesets
@@ -33,38 +29,62 @@ function getRulesets(err, files) {
 }
 function parseRuleset(err, file) {
 	if (err) throw err;
-	// 'this' is bound to the filename
+	// Remove linebreaks, condense spaces
 	const f = file.replace(/[\n\t]/g, "").replace(/\s+/g, " "),
+		// The file should start with a standard <?xml?> declaration
+		// and the root node should be <RuleSet> with a title attribute
 		m = f.match(/^<\?xml(?: version="1.0"| encoding="UTF-8"){2}\?><RuleSet [^>]*title="([^"]+)"/),
+		// 'this' is bound to the string of the filename
 		fm = this.match(/^.*\/([^\/]+)-master\.xml$/),
 		filename = fm[1];
+	// Did we find anything?
 	if(m === null) {
+		// Nope
 		return;
 	}
+	//Yup! Save it.
 	rulesets.set(m[1], filename);
 }
 
-// This section returns a string of basic HTML when the viewer looks for "/rulesets"
-app.get("/rulesets", function(request, response) {
-	var html = "";
-	rulesets.forEach(function(filename, title) {
-		html += "<option value=\"" + filename + "\">" + title + "</option>";
-	});
-	response.send(html);
+
+app.get("/", function(request, response) {
+	var fn = pug.compileFile('views/index.pug', {cache: false});
+	response.send(fn({rulesets: rulesets}));
+	//response.sendFile(__dirname + "/views/index.html");
 });
 
-//
-//
-// Could probably inject this info into the file itself before serving it!
-//
-//
-//
-//
+
+// Requests for XML documents can also be served by Pug documents
+app.get(/^\/.*\.xml/, function(request, response) {
+	//var fn = pug.compileFile('views/xml.pug', {cache: false});
+	var url = request.url,
+		r = /^\/[^.]*(?=.xml$)/,
+		m = url.match(r),
+		fn, pathXml, pathPug;
+	if (m === null) {
+		//Error?
+		return response.sendStatus(500);
+	}
+	pathXml = __dirname + '/public' + m[0] + '.xml';
+	if(fs.existsSync(pathXml)) {
+		return response.sendFile(pathXml);
+	}
+	pathPug = __dirname + '/public' + m[0] + '.pug';
+	if(fs.existsSync(pathPug)) {
+		fn = pug.compileFile(pathPug, {cache: false});
+		response.set('Content-Type', 'text/xml');
+		response.type('text/xml');
+		response.send(fn());
+	} else {
+		response.sendStatus(404);
+	}
+});
 
 
 
-app.get("/debug", function(request, response) {
-	response.send("<xmp>" + JSON.stringify(Array.from(debug)) + "</xmp>");
+app.get(["/debug", "/path/to/file.html"], function(request, response) {
+	//response.send("<xmp>" + JSON.stringify(Array.from(debug)) + "</xmp>");
+	response.send("<xmp>[" + request.baseUrl + "]\n" + request.hostname + "\n" + request.path + "\n" + JSON.stringify(request.route) + "\n" + request.url + "</xmp>");
 });
 
 
