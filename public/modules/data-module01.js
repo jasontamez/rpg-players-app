d3fff21e833cf271692d643da93c8bde551cd5bd
Jason Tamez <druidic@gmail.com>
2020-04-12 01:00:44 -0500

    moving player, character objects to objects-module +add/getPool

--- public/modules/data-module01.js
+++ public/modules/data-module01.js
@@ -5,99 +5,6 @@ var $RPG = window["$RPG"];
 
 
 
-// Define a class for character objects
-//   o = new CharacterObject(objectPlayer, stringRuleset)
-//   o.set("property", value)
-//   o.get("property") => value
-export class CharacterObject {
-	constructor(player, ruleset, characterID) {
-		this.player = player;
-		this.id = characterID;
-		this.ruleset = ruleset;
-		this.data = new Map();
-		this.stats = new Map();
-		this.multistats = new Map();
-		this.bonuses = new Map();
-	}
-	get(dataName) {
-		return this.data.get(dataName);
-	}
-	set(dataName, data) {
-		return this.data.set(dataName, data);
-	}
-	has(dataName) {
-		return this.data.has(dataName);
-	}
-	addStat(id, stat) {
-		return this.stats.set(id, stat);
-	}
-	getStat(id) {
-		return this.stats.get(id);
-	}
-	addMultiStat(id, stat) {
-		return this.multistats.set(id, stat);
-	}
-	getMultiStat(id) {
-		return this.multistats.get(id);
-	}
-	// noteBonus(anyID, stringUndoBonusFunctionName, ...any number of arguments)
-	noteBonus() {
-		var args = Array.from(arguments),
-			id = args.shift(),
-			holder = this.bonuses.get(id) || [];
-		holder.push(args);
-		this.bonuses.set(id, holder);
-		if(id === undefined) {
-			console.log(undefined);
-			console.trace();
-		}
-	}
-	undoBonuses(id) {
-		var bonuses = this.bonuses.get(id);
-		if(bonuses === undefined) {
-			return logErrorText("Unable to find any bonuses labelled \"" + id + "\"");
-		}
-		bonuses.forEach(function(arr) {
-			var n = arr.shift(),
-				undo = $RPG.data.undoBonusMethods[n];
-			if(undo === undefined) {
-				logError(n, "Cannot find undo operation \"" + n + "\"");
-				return;
-			}
-			return undo(...arr);
-		});
-		this.bonuses.delete(id);
-		return true;
-	}
-	//getById?
-}
-
-// Define a class for player objects
-//   TBD
-export class PlayerObject {
-	constructor(playerID) {
-		// TBD
-		this.rulesets = new Map;
-		this.id = playerID;
-		// .set(ruleset, characterID)
-	}
-	makeCharacter(ruleset) {
-		// make an ID
-		var id = "ID",
-			rs = this.rulesets.get(ruleset) || new Map(),
-			char;
-		char = new CharacterObject(this, ruleset, id);
-		rs.set(id, char);
-		this.rulesets.set(ruleset, rs);
-		return char;
-	}
-	getCharacter(ruleset, id) {
-		return this.rulesets.get(ruleset).get(id);
-	}
-	//getById
-}
-
-
 function undoBonus(stat, nombre, type, notation) { //string
 	return stat.removeBonus(nombre, type, notation);
 }
@@ -122,8 +29,6 @@ function emptyPool(stat) {
 
 
 $RPG.ADD("data", {
-	player: PlayerObject,
-	character: CharacterObject,
 	undoBonusMethods: {
 		Bonus: undoBonus,
 		Notation: undoNotation,
--- public/modules/objects-module.js
+++ public/modules/objects-module.js
@@ -5,10 +5,124 @@ import { logErrorText as logError } from "./parsing-logging.js";
 
 
 ////////////////////////////
+/////// DATA OBJECTS ///////
+////////////////////////////
+
+// Each object must define a .toJSON method that ?
+
+// Define a class for player objects
+//   TBD
+export class PlayerObject {
+	constructor(playerID) {
+		// TBD
+		this.rulesets = new Map;
+		this.id = playerID;
+		// .set(ruleset, characterID)
+	}
+	makeCharacter(ruleset) {
+		// make an ID
+		var id = "ID",
+			rs = this.rulesets.get(ruleset) || new Map(),
+			char;
+		char = new CharacterObject(this, ruleset, id);
+		rs.set(id, char);
+		this.rulesets.set(ruleset, rs);
+		return char;
+	}
+	getCharacter(ruleset, id) {
+		return this.rulesets.get(ruleset).get(id);
+	}
+	toJSON(key) {
+		var rs = this.rulesets,
+			rulesets = [];
+		rs.forEach(function(ruleset, rsName) {
+			rulesets.push([rsName, Array.from(ruleset.keys())]);
+		});
+		return {
+			id: this.id,
+			rulesets: rulesets,
+			parser: "PlayerObject"
+		};
+	}
+	//getById
+}
+
+// Define a class for character objects
+//   o = new CharacterObject(objectPlayer, stringRuleset)
+//   o.set("property", value)
+//   o.get("property") => value
+export class CharacterObject {
+	constructor(player, ruleset, characterID) {
+		this.player = player;
+		this.id = characterID;
+		this.ruleset = ruleset;
+		this.data = new Map();
+		this.stats = new Map();
+		this.multistats = new Map();
+		this.pools = new Map();
+		this.bonuses = new Map();
+	get(dataName) {
+		return this.data.get(dataName);
+	}
+	set(dataName, data) {
+		return this.data.set(dataName, data);
+	}
+	has(dataName) {
+		return this.data.has(dataName);
+	}
+	addStat(id, stat) {
+		return this.stats.set(id, stat);
+	}
+	getStat(id) {
+		return this.stats.get(id);
+	}
+	addMultiStat(id, stat) {
+		return this.multistats.set(id, stat);
+	}
+	getMultiStat(id) {
+		return this.multistats.get(id);
+	}
+	addPool(id, pool) {
+		return this.pools.set(id, pool);
+	}
+	getPool(id) {
+		return this.pools.get(id);
+	}
+	noteBonus() {
+		var args = Array.from(arguments),
+			id = args.shift(),
+			holder = this.bonuses.get(id) || [];
+		holder.push(args);
+		this.bonuses.set(id, holder);
+		if(id === undefined) {
+			console.log(undefined);
+			console.trace();
+		}
+	}
+	undoBonuses(id) {
+		var bonuses = this.bonuses.get(id);
+		if(bonuses === undefined) {
+			return logErrorText("Unable to find any bonuses labelled \"" + id + "\"");
+		}
+		bonuses.forEach(function(arr) {
+			var n = arr.shift(),
+				undo = $RPG.data.undoBonusMethods[n];
+			if(undo === undefined) {
+				logError(n, "Cannot find undo operation \"" + n + "\"");
+				return;
+			}
+			return undo(...arr);
+		});
+		this.bonuses.delete(id);
+		return true;
+	}
+}
+
+////////////////////////////
 /////// STAT OBJECTS ///////
 ////////////////////////////
 
-// Each object much define a .toJSON method that 
+// Each object must define a .toJSON method that 
 
 // Basic object, parent of all that follow
 export class ObjectWithAttributes {
@@ -307,6 +421,10 @@ function restoreEquation(key, prop, flagged) {
 
 
 $RPG.ADD("objects", {
+	data: {
+		player: PlayerObject,
+		character: CharacterObject
+	},
 	stats: {
 		ObjectWithAttributes: ObjectWithAttributes,
 		Group: GroupObject,
