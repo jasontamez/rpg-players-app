fdbd76771f41c8ff05eeb946b4daeb413d0aaf17
Jason Tamez <druidic@gmail.com>
2020-04-12 10:08:26 -0500

    temp fix

--- public/rpg-players-app.js
+++ public/rpg-players-app.js
@@ -2,7 +2,8 @@ var $RPG = {
 	UNDO: new Map(),
 	LOG_UNDO: function() {
 		if($RPG.current !== undefined) {
-			let ruleset = this.current.ruleset.name,
+//			let ruleset = this.current.ruleset.name,
+			let ruleset = this.current.character.ruleset,
 				undo = this.UNDO.get(ruleset) || [],
 				args = Array.from(arguments);
 			undo.unshift(args);
